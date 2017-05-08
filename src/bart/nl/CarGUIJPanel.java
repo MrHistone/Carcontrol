185e8736e982eaba1854c64d6e366459e7393466
Bart Jansen <Bart Jansen@SOSDE49>
2017-05-08 14:56:39 +0200

    Small circle inside big circle.

--- src/bart/nl/CarGUIJPanel.java
+++ src/bart/nl/CarGUIJPanel.java
@@ -5,17 +5,15 @@ import javax.swing.JPanel;
 
 public class CarGUIJPanel extends JPanel {
 
-    private int controlX;
-    private int controlY;
+    private int controlX, controlY;
     private int controlSize = 16;
     private int circleSize;
     private int radiusBigCircle;
-    
-    
-    
+    private int centerX, centerY;
+
     @Override
     protected void paintComponent(Graphics g) {
-        if (circleSize == 0){
+        if (circleSize == 0) {
             circleSize = getWidth() - 10;
             radiusBigCircle = circleSize / 2;
         }
@@ -23,71 +21,57 @@ public class CarGUIJPanel extends JPanel {
         super.paintComponent(g);
         g.drawOval(5, 5, circleSize, circleSize);
         g.fillOval(controlX, controlY, controlSize, controlSize);
-        
+
     }
 
-    protected void setXY(int x, int y){
+    protected void setXY(int x, int y) {
         this.controlX = x;
         this.controlY = y;
     }
-    
-    protected int getControlX(){
+
+    protected int getControlX() {
         return controlX;
     }
-    
-    protected int getControlY(){
+
+    protected int getControlY() {
         return controlY;
     }
-    
-    protected void setControlX(int x){
-        this.controlX = x;
-        // Small oval needs to stay in the larger circle.
 
-        // Steps:
-        // Get X relative to center of big circle. (Center = 0)
-        
-        // Get Y relative to center of big circle. (Center = 0)
-        
+    /**
+     * Move the small circle with X and Y.
+     *
+     * @param deltaX The amount X needs to change.
+     * @param deltaY The amount Y needs to change.
+     */
+    public void move(int deltaX, int deltaY) {
+        // Get X and Y relative to center of big circle. (Center = 0)
+        // Take into account the radius of the small circle and tweak a little bit.
+        int relativeX = ((Math.max(controlX + deltaX, centerX) - Math.min(controlX + deltaX, centerX)) + (controlSize / 2)) - 2;
+        int relativeY = ((Math.max(controlY + deltaY, centerY) - Math.min(controlY + deltaY, centerY)) + (controlSize / 2)) - 2;
         // Calculate the hypotenuse with positive values of X and Y.
-        
-        // If hypotenuse gets larger with new X than the 
-        // radius of the big circle, do not make X larger (or smaller).
-        
-        
-        if (controlX < 5){
-            controlX = 5;
+        double hypotenuse = Math.hypot(relativeX, relativeY);
+        // If hypotenuse gets larger with new X or Y than the radius of the big circle, cancel the action.
+        if (hypotenuse > radiusBigCircle) {
+            // X and/or Y cannot get bigger without getting outside the boundary.
+        } else {
+            controlX = controlX + deltaX;
+            controlY = controlY + deltaY;
+            repaint();
         }
-        
-        if (controlX > 5 + circleSize){
-            controlX = circleSize;
-        }
-        
+
     }
-    
-    protected void setControlY(int y){
-        this.controlY = y;
-        // Small oval needs to stay in the larger oval.
-        if (controlY < 5){
-            controlY = 5;
+
+    protected void centerControl() {
+        if (centerX == 0) {
+            controlX = getWidth() / 2;
+            controlX = controlX - controlSize / 2;
+            controlY = getHeight() / 2;
+            controlY = controlY - controlSize / 2;
+            centerX = controlX;
+            centerY = controlY;
         }
-        
-        if (controlY > 5 + circleSize){
-            controlY = circleSize;
-        }
-        
-        
-        
-        
+        controlX = centerX;
+        controlY = centerY;
     }
-    
-    
-    protected void centerControl(){
-        controlX = getWidth() / 2;
-        controlX = controlX - controlSize / 2;
-        controlY = getHeight() / 2;
-        controlY = controlY - controlSize / 2;
-    }
-    
-    
-    
+
 }
